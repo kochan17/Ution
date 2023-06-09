25de4847d0be43b992716e0d8f0e654bcd432f72
Kota <i7811832616@gmail.com>
2023-06-09 13:16:05 +0900

    Routingエラー解消2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :conversations, only: [:create]
-  get 'conversations/new/:persona_id', to: 'conversations#new', as: 'new_conversation'
+  post 'conversations/new/:persona_id', to: 'conversations#new', as: 'new_conversation'
 end
