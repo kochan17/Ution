2672dfcdc3f99ae35eb0424c415c4069dff0b1d9
Kota <i7811832616@gmail.com>
2023-06-10 18:56:58 +0900

    Update openai_service.rb

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,24 +1,32 @@
-require 'openai'
+require 'net/http'
+require 'uri'
+require 'json'
 
 class OpenaiService
   def initialize
-    OpenAI.api_key = ENV["OPENAI_API_KEY"]
+    @api_key = ENV["OPENAI_API_KEY"]
   end
 
   def chat(message)
-    response = OpenAI::ChatCompletion.create(
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant."
-        },
-        {
-          role: "user",
-          content: message
-        }
-      ]
-    )
-    response['choices'][0]['message']['content']
+    uri = URI.parse("https://api.openai.com/v1/engines/davinci-codex/completions")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer #{@api_key}"
+    request.body = JSON.dump({
+      "prompt" => message,
+      "max_tokens" => 60
+    })
+  
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+  
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  
+    json = JSON.parse(response.body)
+    puts json # レスポンスの内容をログに出力
+    json['choices'][0]['text'].strip
   end
 end
